c5df1955a75b774751f74f0d61faf7ca6a890106
Hui Wan <hui.wan@pnnl.gov>
2021-03-24 15:57:24 -0700

    fix syntax errors

--- components/cam/src/physics/cam/conditional_diag.F90
+++ components/cam/src/physics/cam/conditional_diag.F90
@@ -4,9 +4,10 @@ module conditional_diag
 ! History:
 !  First version by Hui Wan, PNNL, March 2021
 !-------------------------------------------------
-  use shr_kind_mod, only: r8 => shr_kind_r8
-  use spmd_utils,   only: masterproc
-  use cam_logfile,  only: iulog
+  use shr_kind_mod,   only: r8 => shr_kind_r8
+  use spmd_utils,     only: masterproc
+  use cam_logfile,    only: iulog
+  use cam_abortutils, only: endrun
 
   implicit none
 
@@ -15,12 +16,13 @@ module conditional_diag
   ! Derived types
 
   public cnd_diag_info_t
-  public cnd_diag_t, cnd_diag
+  public cnd_diag_t, cnd_diag_info
 
   ! Subroutines
 
   public conditional_diag_alloc
  !public conditional_diag_dealloc
+  public conditional_diag_readnl
 
   ! module parameters
 
@@ -80,10 +82,10 @@ module conditional_diag
   !-------------------------------------------------------------------------------
   type cnd_diag_t
 
-    real(r8),                   allocatable :: metric_val(:,:)  ! shape = (pcols, info%metric_nver(imetric))
-    type(snapshot_and_tendency),allocatable :: fld_1lev (:)     ! shape = (info%nfld_1lev)
-    type(snapshot_and_tendency),allocatable :: fld_nlev (:)     ! shape = (info%nfld_nlev)
-    type(snapshot_and_tendency),allocatable :: fld_nlevp(:)     ! shape = (info%nfld_nlevp1)
+    real(r8),                     allocatable :: metric (:,:)     ! shape = (pcols, info%metric_nver(imetric))
+    type(snapshot_and_tendency_t),allocatable :: fld_1lev (:)     ! shape = (info%nfld_1lev)
+    type(snapshot_and_tendency_t),allocatable :: fld_nlev (:)     ! shape = (info%nfld_nlev)
+    type(snapshot_and_tendency_t),allocatable :: fld_nlevp(:)     ! shape = (info%nfld_nlevp1)
 
   end type cnd_diag_t
 
@@ -102,10 +104,11 @@ module conditional_diag
 !===============================================================================
   type(cnd_diag_info_t) :: cnd_diag_info
 
+contains
 !===============================================================================
 ! Procedures
 !===============================================================================
-subroutine conditiona_diag_readnl(nlfile)
+subroutine conditional_diag_readnl(nlfile)
 
 !  use namelist_utils,  only: find_group_name
 !  use units,           only: getunit, freeunit
@@ -120,25 +123,28 @@ subroutine conditiona_diag_readnl(nlfile)
 
    ! Local variables for reading namelist
 
-   character(len=metric_name_maxlen)   :: metric_name(nmetric_max) = ' '
+   character(len=metric_name_maxlen)   :: metric_name(nmetric_max)
    integer  :: metric_nver(nmetric_max)
    integer  :: metric_cmpr_type(nmetric_max)
    real(r8) :: metric_threshold(nmetric_max)
 
-   character(len=physproc_name_maxlen) :: physproc_name(nphysproc_max) = ' '
+   character(len=physproc_name_maxlen) :: physproc_name(nphysproc_max)
 
-   character(len=fld_name_maxlen) :: fld_name_1lev (nfld_max) = ' '
-   character(len=fld_name_maxlen) :: fld_name_nlev (nfld_max) = ' '
-   character(len=fld_name_maxlen) :: fld_name_nlevp(nfld_max) = ' '
+   character(len=fld_name_maxlen) :: fld_name_1lev (nfld_max)
+   character(len=fld_name_maxlen) :: fld_name_nlev (nfld_max)
+   character(len=fld_name_maxlen) :: fld_name_nlevp(nfld_max)
 
-   integer :: nmetric = 0
-   integer :: nphysproc = 0
-   integer :: nfld = 0
+   logical :: l_output_state, l_output_tend
+
+   integer :: nmetric
+   integer :: nphysproc
+   integer :: nfld
 
    integer :: ii
 
    namelist /conditional_diag_nl/  metric_name, metric_nver, metric_cmpr_type, metric_threshold, &
-                                   physproc_name, fld_name_1lev, fld_name_nlev, fld_name_nlevp
+                                   physproc_name, fld_name_1lev, fld_name_nlev, fld_name_nlevp,  &
+                                   l_output_state, l_output_tend
 
    !----------------------------------------
    !  Default values
@@ -163,7 +169,7 @@ subroutine conditiona_diag_readnl(nlfile)
    metric_name     (1:3) = (/"CAPE","SSATW","SSATI"/)
    metric_nver     (1:3) = (/1,     72,     72/)
    metric_cmpr_type(1:3) = (/GT,    GT,     GT/)
-   metric_metric_threshold(1:3) = (/0._wp, 0._wp, 0._wp/)
+   metric_threshold(1:3) = (/0._r8, 0._r8, 0._r8/)
 
    nphysproc = 2
    physproc_name(1) = "zm_conv_tend"
@@ -195,7 +201,10 @@ subroutine conditiona_diag_readnl(nlfile)
 
    allocate( cnd_diag_info%metric_name(nmetric), stat=ierr)
    if ( ierr /= 0 ) call endrun('conditional_diag_readnl: allocation error for cnd_diag_info%metric_name')
-   cnd_diag_info%metric_name(1:nmetric) = trim(adjustl(metric_name(1:nmetric)))
+
+   do ii = 1,nmetric
+      cnd_diag_info%metric_name(ii) = trim(adjustl(metric_name(ii)))
+   end do
 
    allocate( cnd_diag_info%metric_nver(nmetric), stat=ierr)
    if ( ierr /= 0 ) call endrun('conditional_diag_readnl: allocation error for cnd_diag_info%metric_nver')
@@ -221,7 +230,9 @@ subroutine conditiona_diag_readnl(nlfile)
 
    allocate( cnd_diag_info%physproc_name(nphysproc), stat=ierr)
    if ( ierr /= 0 ) call endrun('conditional_diag_readnl: allocation error for cnd_diag_info%physproc_name')
-   cnd_diag_info%physproc_name(1:nphysproc) = trim(adjustl(physproc_name(1:nphysproc)))
+   do ii = 1,nphysproc
+      cnd_diag_info%physproc_name(ii) = trim(adjustl(physproc_name(ii)))
+   end do
 
    ! fields with 1 vertitical level
 
@@ -235,7 +246,9 @@ subroutine conditiona_diag_readnl(nlfile)
 
    allocate( cnd_diag_info%fld_name_1lev(nfld), stat=ierr)
    if ( ierr /= 0 ) call endrun('conditional_diag_readnl: allocation error for cnd_diag_info%fld_name_1lev')
-   cnd_diag_info%fld_name_1lev(1:nfld) = trim(adjustl(fld_name_1lev(1:nfld)))
+   do ii = 1,nfld
+      cnd_diag_info%fld_name_1lev(ii) = trim(adjustl(fld_name_1lev(ii)))
+   end do
 
    ! fields with nlev vertitical levels 
 
@@ -249,7 +262,9 @@ subroutine conditiona_diag_readnl(nlfile)
 
    allocate( cnd_diag_info%fld_name_nlev(nfld), stat=ierr)
    if ( ierr /= 0 ) call endrun('conditional_diag_readnl: allocation error for cnd_diag_info%fld_name_nlev')
-   cnd_diag_info%fld_name_nlev(1:nfld) = trim(adjustl(fld_name_nlev(1:nfld)))
+   do ii = 1,nfld
+      cnd_diag_info%fld_name_nlev(ii) = trim(adjustl(fld_name_nlev(ii)))
+   end do
 
    ! fields with nlev+1 vertitical levels 
 
@@ -263,7 +278,12 @@ subroutine conditiona_diag_readnl(nlfile)
 
    allocate( cnd_diag_info%fld_name_nlevp(nfld), stat=ierr)
    if ( ierr /= 0 ) call endrun('conditional_diag_readnl: allocation error for cnd_diag_info%fld_name_nlevp')
-   cnd_diag_info%fld_name_nlevp(1:nfld) = trim(adjustl(fld_name_nlevp(1:nfld)))
+   do ii = 1,nfld
+      cnd_diag_info%fld_name_nlevp(ii) = trim(adjustl(fld_name_nlevp(ii)))
+   end do
+
+   cnd_diag_info%l_output_state = l_output_state
+   cnd_diag_info%l_output_tend  = l_output_tend 
 
    !-----------------------------------------------
    ! Send information to log file
@@ -311,7 +331,7 @@ subroutine conditiona_diag_readnl(nlfile)
 
       write(iulog,*)
       write(iulog,'(4x,a40)')'field w/ nlev+1 vertical levels'
-      do ii = 1,cnd_diag_info%nfld_nlevp1
+      do ii = 1,cnd_diag_info%nfld_nlevp
          write(iulog,'(i4.3,a40)') ii, cnd_diag_info%fld_name_nlevp(ii)
       end do
 
@@ -319,7 +339,10 @@ subroutine conditiona_diag_readnl(nlfile)
       write(iulog,*)  'l_output_tend  = ',l_output_tend
       write(iulog,*)' --------------------------------------------------'
 
-end subroutine conditiona_diag_readnl
+   end if
+  end if
+
+end subroutine conditional_diag_readnl
 
 !===============================================================================
 subroutine conditional_diag_alloc( psetcols, pver, info, diag )
@@ -328,7 +351,7 @@ subroutine conditional_diag_alloc( psetcols, pver, info, diag )
 
   integer, intent(in) :: psetcols, pver
   type(cnd_diag_info_t), intent(in) :: info
-  type(cnd_diag_t),   intent(inout) :: diag
+  type(cnd_diag_t),pointer,intent(inout) :: diag(:)
 
   integer :: nmetric, im
   integer :: nfld_1lev, nfld_nlev, nfld_nlevp, ifld
@@ -365,18 +388,18 @@ subroutine conditional_diag_alloc( psetcols, pver, info, diag )
         ! field values and tendencies of each diagnostic variables with 1 vertical levels
         do ifld = 1, nfld_1lev
 
-           allocate( diag(im)%fld_1lev(ifld)% cur(npsetcols,1), stat=ierr)
+           allocate( diag(im)%fld_1lev(ifld)% cur(psetcols,1), stat=ierr)
            if ( ierr /= 0 ) call endrun('conditional_diag_allocate error: allocation error for diag%fld_1lev%cur')
 
-           allocate( diag(im)%fld_1lev(ifld)% val(npsetcols,1,nphysproc), stat=ierr)
+           allocate( diag(im)%fld_1lev(ifld)% val(psetcols,1,nphysproc), stat=ierr)
            if ( ierr /= 0 ) call endrun('conditional_diag_allocate error: allocation error for diag%fld_1lev%val')
 
-           allocate( diag(im)%fld_1lev(ifld)% tnd(npsetcols,1,nphysproc), stat=ierr)
+           allocate( diag(im)%fld_1lev(ifld)% tnd(psetcols,1,nphysproc), stat=ierr)
            if ( ierr /= 0 ) call endrun('conditional_diag_allocate error: allocation error for diag%fld_1lev%tnd')
 
-           diag(im)%fld_1lev(ifld)% cur(:,:) = inf
-           diag(im)%fld_1lev(ifld)% val(:,:) = inf
-           diag(im)%fld_1lev(ifld)% tnd(:,:) = inf
+           diag(im)%fld_1lev(ifld)% cur(:,:)   = inf
+           diag(im)%fld_1lev(ifld)% val(:,:,:) = inf
+           diag(im)%fld_1lev(ifld)% tnd(:,:,:) = inf
 
         end do !ifld
 
@@ -395,18 +418,18 @@ subroutine conditional_diag_alloc( psetcols, pver, info, diag )
          ! field values and tendencies of each diagnostic variables with 1 vertical levels
          do ifld = 1, nfld_nlev
 
-            allocate( diag(im)%fld_nlev(ifld)% cur(npsetcols,pver), stat=ierr)
+            allocate( diag(im)%fld_nlev(ifld)% cur(psetcols,pver), stat=ierr)
             if ( ierr /= 0 ) call endrun('conditional_diag_alloc error: allocation error for diag%fld_nlev%cur')
 
-            allocate( diag(im)%fld_nlev(ifld)% val(npsetcols,pver,nphysproc), stat=ierr)
+            allocate( diag(im)%fld_nlev(ifld)% val(psetcols,pver,nphysproc), stat=ierr)
             if ( ierr /= 0 ) call endrun('conditional_diag_alloc error: allocation error for diag%fld_nlev%val')
 
-            allocate( diag(im)%fld_nlev(ifld)% tnd(npsetcols,pver,nphysproc), stat=ierr)
+            allocate( diag(im)%fld_nlev(ifld)% tnd(psetcols,pver,nphysproc), stat=ierr)
             if ( ierr /= 0 ) call endrun('conditional_diag_alloc error: allocation error for diag%fld_nlev%tnd')
 
-            diag(im)%fld_nlev(ifld)% cur(:,:) = inf
-            diag(im)%fld_nlev(ifld)% val(:,:) = inf
-            diag(im)%fld_nlev(ifld)% tnd(:,:) = inf
+            diag(im)%fld_nlev(ifld)% cur(:,:)   = inf
+            diag(im)%fld_nlev(ifld)% val(:,:,:) = inf
+            diag(im)%fld_nlev(ifld)% tnd(:,:,:) = inf
 
          end do !ifld
 
@@ -424,18 +447,18 @@ subroutine conditional_diag_alloc( psetcols, pver, info, diag )
         ! field values and tendencies of each diagnostic variables with 1 vertical levels
         do ifld = 1, nfld_nlevp
 
-           allocate( diag(im)%fld_nlevp(ifld)% cur(npsetcols,pver+1), stat=ierr)
+           allocate( diag(im)%fld_nlevp(ifld)% cur(psetcols,pver+1), stat=ierr)
            if ( ierr /= 0 ) call endrun('conditional_diag_alloc error: allocation error for diag%fld_nlevp%cur')
 
-           allocate( diag(im)%fld_nlevp(ifld)% val(npsetcols,pver+1,nphysproc), stat=ierr)
+           allocate( diag(im)%fld_nlevp(ifld)% val(psetcols,pver+1,nphysproc), stat=ierr)
            if ( ierr /= 0 ) call endrun('conditional_diag_alloc error: allocation error for diag%fld_nlevp%val')
 
-           allocate( diag(im)%fld_nlevp(ifld)% tnd(npsetcols,pver+1,nphysproc), stat=ierr)
+           allocate( diag(im)%fld_nlevp(ifld)% tnd(psetcols,pver+1,nphysproc), stat=ierr)
            if ( ierr /= 0 ) call endrun('conditional_diag_alloc error: allocation error for diag%fld_nlevp%tnd')
 
-           diag(im)%fld_nlevp(ifld)% cur(:,:) = inf
-           diag(im)%fld_nlevp(ifld)% val(:,:) = inf
-           diag(im)%fld_nlevp(ifld)% tnd(:,:) = inf
+           diag(im)%fld_nlevp(ifld)% cur(:,:)   = inf
+           diag(im)%fld_nlevp(ifld)% val(:,:,:) = inf
+           diag(im)%fld_nlevp(ifld)% tnd(:,:,:) = inf
 
         end do !ifld
       end do   !im
